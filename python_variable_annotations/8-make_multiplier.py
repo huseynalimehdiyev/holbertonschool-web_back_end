90e98c39d0ee0d2491d58a93729927f2cb9e63e6
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 03:38:14 +0400

    Advanced Python:Functions

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float,) -> Callable[[float], float]
+    """Function"""
+
+    def multiply(x: float) -> float:
+        return x * multiplier
+
+    return multiply
