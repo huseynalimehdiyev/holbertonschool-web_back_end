abe1746202dcec4676919061a3a4aed1a87b685e
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 03:26:59 +0400

    Advanced Python:Functions

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module"""
 
+from typing import List
 
-def sum_list(input_list: list) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Function"""
 
     for i in input_list:
