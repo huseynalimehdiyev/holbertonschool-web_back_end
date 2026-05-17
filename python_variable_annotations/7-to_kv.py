6c9e0eea5a731e52ca36986f0071bb6624aaa175
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 03:34:32 +0400

    Advanced Python:Functions

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Module"""
 
-from typing import List, Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function"""
 
     return (k, float(v ** 2))
