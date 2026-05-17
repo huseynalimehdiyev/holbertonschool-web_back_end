81ee7783f2cc098d851900bf5af2efe203819de5
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 03:13:14 +0400

    Advanced Python:Functions

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""
-This module provides a function that adds two floating point numbers.
-"""
+"""This module provides a function to add two floats."""
 
 def add(a: float, b: float) -> float:
+    """Function"""
     return a + b
