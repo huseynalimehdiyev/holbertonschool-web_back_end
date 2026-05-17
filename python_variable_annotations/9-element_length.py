a03508f465cc56304d5d12e5149d3bdb02040614
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 03:41:28 +0400

    Advanced Python:Functions

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Module"""
 
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable,Sequence
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function"""
 
     return [(i, len(i)) for i in lst]
