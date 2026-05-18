1d12652d01701638fb8f03579ee8d40dd03f542f
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 12:29:17 +0400

    Python tasks

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -6,16 +6,16 @@ This module provides a function that safely gets a value from a dictionary.
 from typing import TypeVar, Mapping, Optional
 
 
-K = TypeVar('K')
-V = TypeVar('V')
+T = TypeVar('T')
 
 
 def safely_get_value(
-    dct: Mapping[K, V],
-    key: K,
-    default: Optional[V] = None
-) -> Optional[V]:
+    dct: Mapping,
+    key: Any,
+    default: Optional[T] = None
+) -> Any | T:
     """Function"""
+
     if key in dct:
         return dct[key]
     else:
