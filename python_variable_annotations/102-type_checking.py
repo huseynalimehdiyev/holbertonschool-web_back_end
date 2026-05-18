798c2d4eeae30b25ff92b6bfadbc9e3bd4865aad
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-18 12:40:58 +0400

    Python tasks

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module"""
+
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+    """Function"""
+
+    zoomed_in = tuple(
+        item for item in lst
+        for i in range(factor)
+    )
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
