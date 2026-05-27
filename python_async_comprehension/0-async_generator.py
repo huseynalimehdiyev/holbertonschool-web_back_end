d8adbc22c1aa1c3f7c91a77a3fac96d8849eba20
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-28 02:44:19 +0400

    Python

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Function"""
 
     for _ in range(10):
