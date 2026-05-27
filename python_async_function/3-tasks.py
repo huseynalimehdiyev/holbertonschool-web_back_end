0ab92f59bc3bdb4f47710d42387850ef543d6196
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-28 02:25:42 +0400

    Python

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module"""
+
+import asyncio
+import time
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Function"""
+
+    return asyncio.create_task(wait_random(max_delay))
