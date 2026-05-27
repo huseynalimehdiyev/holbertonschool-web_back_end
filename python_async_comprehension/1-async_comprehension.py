b425aacfa8ae05a47b0c6854934691de3f02f992
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-28 02:50:19 +0400

    Python

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module"""
+
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
@@ -7,5 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Function"""
-    
+
     return [i async for i in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module"""
+
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
