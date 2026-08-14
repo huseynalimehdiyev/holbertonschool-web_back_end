d359ac9857945347527f4f4e4be031fc80b5d480
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-08-15 03:35:53 +0400

    Hello

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,24 +4,27 @@ const fs = require('fs');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+    res.end('Hello Holberton School!');
+    return;
+  }
 
+  if (req.url === '/students') {
     fs.readFile(database, 'utf8', (error, data) => {
       if (error) {
-        res.end('Cannot load the database\n');
+        res.statusCode = 500;
+        res.end('Cannot load the database');
         return;
       }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const lines = data
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
       const students = lines.slice(1);
-
-      res.write(`Number of students: ${students.length}\n`);
-
       const fields = {};
 
       students.forEach((student) => {
@@ -36,17 +39,19 @@ const app = http.createServer((req, res) => {
         fields[field].push(firstname);
       });
 
+      let result = 'This is the list of our students\n';
+      result += `Number of students: ${students.length}\n`;
+
       Object.keys(fields).forEach((field) => {
-        res.write(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`
-        );
+        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
       });
 
-      res.end();
+      res.end(result.trim());
     });
-  } else {
-    res.end();
+    return;
   }
+
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
