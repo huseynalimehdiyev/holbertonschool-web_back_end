89244ca6b07e46b38e42421063bc834110c4dbca
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-08-15 03:30:44 +0400

    Hello

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (name) => {
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
   console.log(`Your name is: ${name}`);
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
