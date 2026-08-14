d781b1d3a94e92e05862611bb41c48ca0d89b77c
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-08-15 03:44:20 +0400

    Hello

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,50 +1,58 @@
-const express = require('express');
+const http = require('http');
 const fs = require('fs');
 
-const app = express();
 const database = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!');
-});
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-app.get('/students', (req, res) => {
-  fs.readFile(database, 'utf8', (error, data) => {
-    if (error) {
-      res.status(500).type('text').send(
-        'This is the list of our students\nCannot load the database',
-      );
-      return;
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+    return;
+  }
 
-    const lines = data
-      .split('\n')
-      .filter((line) => line.trim() !== '');
-
-    const students = lines.slice(1);
-    const fields = {};
-
-    students.forEach((student) => {
-      const columns = student.split(',');
-      const firstname = columns[0];
-      const field = columns[3];
-
-      if (!fields[field]) {
-        fields[field] = [];
+  if (req.url === '/students') {
+    fs.readFile(database, 'utf8', (error, data) => {
+      if (error) {
+        res.statusCode = 500;
+        res.end('This is the list of our students\nCannot load the database');
+        return;
       }
 
-      fields[field].push(firstname);
+      const lines = data
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+      const fields = {};
+
+      students.forEach((student) => {
+        const columns = student.split(',');
+        const firstname = columns[0];
+        const field = columns[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      let result = 'This is the list of our students\n';
+      result += `Number of students: ${students.length}\n`;
+
+      Object.keys(fields).forEach((field) => {
+        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+      });
+
+      res.end(result.trim());
     });
 
-    let result = 'This is the list of our students\n';
-    result += `Number of students: ${students.length}\n`;
+    return;
+  }
 
-    Object.keys(fields).forEach((field) => {
-      result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-    });
-
-    res.type('text').send(result.trim());
-  });
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
